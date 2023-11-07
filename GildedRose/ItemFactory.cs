57b43cda44823c264cca77dd0965ea8bdb2a594d
David Barcón <dbarcon@domingoalonsogroup.com>
2023-11-07 12:49:26 +0000

    Created ItemFactory Class

---
+++ GildedRose/ItemFactory.cs
@@ -0,0 +1,20 @@
+﻿namespace GildedRose
+{
+    public class ItemFactory
+    {
+        public static Item CreateItem(string name, int sellIn, int quality)
+        {
+            switch (name)
+            {
+                case "Aged Brie":
+                    return new BrieItem(name, sellIn, quality);
+                case "Backstage passes to a TAFKAL80ETC concert":
+                    return new PassItem(name, sellIn, quality);
+                case "Sulfuras, Hand of Ragnaros":
+                    return new SulfurasItem(name, 15, 80);
+                default:
+                    return new NormalItem(name, sellIn, quality);
+            }
+        }
+    }
+}
