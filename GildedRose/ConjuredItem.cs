6f1f565c7b940c410634291d652f3b41e944420f
David Barcón <dbarcon@domingoalonsogroup.com>
2023-11-07 14:53:24 +0000

    green: conjure_item_quality_degrades_twice_as_fast

---
+++ GildedRose/ConjuredItem.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GildedRose
+{
+    internal class ConjuredItem: Item, UpdateInterface
+    {
+        public ConjuredItem(string name, int SellIn, int quality)
+        {
+            this.Name = name;
+            this.Quality = quality;
+            this.SellIn= SellIn;
+        }
+
+        void UpdateInterface.Update()
+        {
+            if (Quality > 0)
+            {
+                if (SellIn >= 0) Quality -= 2;
+                else Quality -= 4;
+            }
+
+            SellIn--;
+        }
+
+}
+}
--- GildedRose/ItemFactory.cs
+++ GildedRose/ItemFactory.cs
@@ -12,6 +12,8 @@
                     return new PassItem(name, sellIn, quality);
                 case "Sulfuras, Hand of Ragnaros":
                     return new SulfurasItem(name, 15, 80);
+                case "Conjured":
+                    return new ConjuredItem(name, sellIn, quality);
                 default:
                     return new NormalItem(name, sellIn, quality);
             }
